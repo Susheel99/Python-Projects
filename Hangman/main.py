06808ef294c2bbe4950efacfd2919108b67e75ea
Susheel99 <asusheel23b@gmail.com>
2020-12-15 12:36:31 +0530

    Loaded images into the project succesfully

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,8 +1,41 @@
-import pandas as pd
+import pygame
 
-cnt = int(input("Enter the number"))
+# setup
+pygame.init()
+width, height = 800, 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Hangman Game!!")
 
-print(cnt)
+# load images
+images = []
+for i in range(7):
+    image = pygame.image.load("images/hangman" + str(i) + ".png")
+    images.append(image)
+print(images)
 
-s = pd.Series([1, 2, 3])
-print(s)
+# game variables
+hangman_status = 6
+
+# colors
+white = (255, 255, 255)
+
+# setup game loop
+fps = 60
+clock = pygame.time.Clock()
+run = True
+
+while run:
+    clock.tick(fps)
+
+    win.fill(white)
+    win.blit(images[hangman_status], (150, 100))
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+
+pygame.quit()

f20fc23dfd970a99d6996b7f449bb7f589c8bbd0
Susheel99 <asusheel23b@gmail.com>
2020-12-17 13:54:46 +0530

    initialized packages to the project

---
+++ web-crawler/main.py
@@ -0,0 +1,10 @@
+import os
+
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print("Creating project " + directory)
+        os.makedirs(directory)
+
+
+create_project_dir("boston")

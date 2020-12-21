943736b81d6f94dbba199ca7175764d7652acf79
Susheel Arukonda <asusheel23b@gmail.com>
2020-12-21 11:11:00 +0530

    Added mysql database

--- student-managment/main.py
+++ student-managment/main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import ttk
 import pymysql
-import stm as stm
 
 
 class Student:
@@ -9,7 +8,8 @@ class Student:
         self.root = rt
         self.root.title("Student Management System")
         self.root.geometry("700x500")
-        title = Label(self.root, text="Student File Management", bd=10, relief=GROOVE, font=("times new roman", 40, "bold"), bg='crimson', fg='grey')
+        title = Label(self.root, text="Student File Management", bd=10, relief=GROOVE,
+                      font=("times new roman", 40, "bold"), bg='crimson', fg='grey')
         title.pack(side=TOP, fill=X)
 
         # Variables
@@ -20,48 +20,53 @@ class Student:
         self.contact_var = StringVar()
         self.dob_var = StringVar()
 
-
         # Manage frame
         Manage_frame = Frame(self.root, bd=4, relief=RIDGE, bg="CRIMSON")
         Manage_frame.place(x=20, y=100, width=450, height=650)
 
-        m_title = Label(Manage_frame, text="Manage students", font=("times new roman", 30, "bold"),bg="CRIMSON", fg='white')
+        m_title = Label(Manage_frame, text="Manage students", font=("times new roman", 30, "bold"), bg="CRIMSON",
+                        fg='white')
         m_title.grid(row=0, columnspan=2, pady=20)
 
         # roll no
-        lbl = Label(Manage_frame, text="Roll no.", font=("times new roman", 20, "bold"), bg="CRIMSON",fg='white')
+        lbl = Label(Manage_frame, text="Roll no.", font=("times new roman", 20, "bold"), bg="CRIMSON", fg='white')
         lbl.grid(row=1, column=0, pady=10, padx=20, sticky='w')
 
-        txt = Entry(Manage_frame, textvariable=self.Roll_no_var, font=("times new roman", 15, "bold"), bd=5, relief=GROOVE)
+        txt = Entry(Manage_frame, textvariable=self.Roll_no_var, font=("times new roman", 15, "bold"), bd=5,
+                    relief=GROOVE)
         txt.grid(row=1, column=1, pady=10, padx=20, sticky='w')
 
         # name
         lbl2 = Label(Manage_frame, text="Name", font=("times new roman", 20, "bold"), bg="CRIMSON", fg='white')
         lbl2.grid(row=2, column=0, pady=10, padx=20, sticky='w')
 
-        txt2 = Entry(Manage_frame, textvariable=self.name_var, font=("times new roman", 15, "bold"), bd=5, relief=GROOVE)
+        txt2 = Entry(Manage_frame, textvariable=self.name_var, font=("times new roman", 15, "bold"), bd=5,
+                     relief=GROOVE)
         txt2.grid(row=2, column=1, pady=10, padx=20, sticky='w')
 
         # email
         lbl3 = Label(Manage_frame, text="Email", font=("times new roman", 20, "bold"), bg="CRIMSON", fg='white')
         lbl3.grid(row=3, column=0, pady=10, padx=20, sticky='w')
 
-        txt3 = Entry(Manage_frame, textvariable=self.email_var, font=("times new roman", 15, "bold"), bd=5, relief=GROOVE)
+        txt3 = Entry(Manage_frame, textvariable=self.email_var, font=("times new roman", 15, "bold"), bd=5,
+                     relief=GROOVE)
         txt3.grid(row=3, column=1, pady=10, padx=20, sticky='w')
 
         # gender
         lbl4 = Label(Manage_frame, text="Gender", font=("times new roman", 20, "bold"), bg="CRIMSON", fg='white')
         lbl4.grid(row=4, column=0, pady=10, padx=20, sticky='w')
 
-        combo_gender = ttk.Combobox(Manage_frame, textvariable=self.gender_var, font=("times new roman", 13, "bold"),state='readonly')
+        combo_gender = ttk.Combobox(Manage_frame, textvariable=self.gender_var, font=("times new roman", 13, "bold"),
+                                    state='readonly')
         combo_gender['values'] = ('male', 'female', 'other')
-        combo_gender.grid(row=4, column=1,pady=10, padx=20)
+        combo_gender.grid(row=4, column=1, pady=10, padx=20)
 
         # contact
         lbl5 = Label(Manage_frame, text="Contact", font=("times new roman", 20, "bold"), bg="CRIMSON", fg='white')
         lbl5.grid(row=5, column=0, pady=10, padx=20, sticky='w')
 
-        txt5 = Entry(Manage_frame, textvariable=self.contact_var, font=("times new roman", 15, "bold"), bd=5, relief=GROOVE)
+        txt5 = Entry(Manage_frame, textvariable=self.contact_var, font=("times new roman", 15, "bold"), bd=5,
+                     relief=GROOVE)
         txt5.grid(row=5, column=1, pady=10, padx=20, sticky='w')
 
         # dob
@@ -71,27 +76,22 @@ class Student:
         txt6 = Entry(Manage_frame, textvariable=self.dob_var, font=("times new roman", 15, "bold"), bd=5, relief=GROOVE)
         txt6.grid(row=6, column=1, pady=10, padx=20, sticky='w')
 
-        #address
+        # address
         lbl7 = Label(Manage_frame, text="Address", font=("times new roman", 20, "bold"), bg="CRIMSON", fg='white')
         lbl7.grid(row=7, column=0, pady=10, padx=20, sticky='w')
 
         self.txt7 = Text(Manage_frame, width=30, height=4, font=("", 10))
         self.txt7.grid(row=7, column=1, pady=10, padx=20, sticky='w')
 
-
-
         # **buttons**
         btn_frame = Frame(self.root, bd=4, relief=RIDGE, bg="CRIMSON")
         btn_frame.place(x=30, y=650, width=420)
 
-        addbtn = Button(btn_frame, text='Add', width=10, command=self.add_students).grid(row=0, column=0, padx=10, pady=10)
+        addbtn = Button(btn_frame, text='Add', width=10, command=self.add_students).grid(row=0, column=0, padx=10,
+                                                                                         pady=10)
         upadatebtn = Button(btn_frame, text='Update', width=10).grid(row=0, column=1, padx=10, pady=10)
         deletebtn = Button(btn_frame, text='Delete', width=10).grid(row=0, column=2, padx=10, pady=10)
         clearbtn = Button(btn_frame, text='Clear', width=10).grid(row=0, column=3, padx=10, pady=10)
-
-
-
-
 
         # _______Data frame_______
         data_frame = Frame(self.root, bd=4, relief=RIDGE, bg="CRIMSON")
@@ -115,9 +115,11 @@ class Student:
         table_frame = Frame(data_frame, bd=4, relief=RIDGE, bg="CRIMSON")
         table_frame.place(x=10, y=70, width=760, height=450)
 
-        scroll_x = Scrollbar(table_frame,orient=HORIZONTAL)
-        scroll_y = Scrollbar(table_frame,orient=VERTICAL)
-        student_table = ttk.Treeview(table_frame,column=('roll', 'name', 'email', 'gender', 'contact', 'dob', 'address'), xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
+        scroll_x = Scrollbar(table_frame, orient=HORIZONTAL)
+        scroll_y = Scrollbar(table_frame, orient=VERTICAL)
+        student_table = ttk.Treeview(table_frame,
+                                     column=('roll', 'name', 'email', 'gender', 'contact', 'dob', 'address'),
+                                     xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
         scroll_y.pack(side=RIGHT, fill=Y)
         scroll_x.pack(side=BOTTOM, fill=X)
         scroll_y.config(command=student_table.yview)
@@ -143,18 +145,15 @@ class Student:
     def add_students(self):
         con = pymysql.connect(host="localhost", user="root", password="", database="stm")
         cur = con.cursor()
-        cur.execute("insert into students values(%s,%s,%s,%s,%s,%s,%s)",(self.Roll_no_var.get(),
-                                                                         self.name_var.get(),
-                                                                         self.email_var.get(),
-                                                                         self.gender_var.get(),
-                                                                         self.contact_var.get(),
-                                                                         self.dob_var.get(),
-                                                                         self.txt7.get('1.0', END)))
+        cur.execute("insert into students values(%s,%s,%s,%s,%s,%s,%s)", (self.Roll_no_var.get(),
+                                                                          self.name_var.get(),
+                                                                          self.email_var.get(),
+                                                                          self.gender_var.get(),
+                                                                          self.contact_var.get(),
+                                                                          self.dob_var.get(),
+                                                                          self.txt7.get('1.0', END)))
         con.commit()
         con.close()
-
-
-
 
 
 root = Tk()
